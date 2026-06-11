0c9b4aae638f98d85972dc8488e2ae19da56edfc
SaatvikLodha <saatviklodha@gmail.com>
2026-06-11 15:43:01 -0500

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,11 @@
+#Store 5 different birthdays in 5 different variables and then print all of it
+dad="July", 11
+mom="May" , 7
+me="September" , 7
+brother="July" , 26
+randomperson="January" , 1
+print(dad)
+print(mom)
+print(me)
+print(brother)
+print(randomperson)
